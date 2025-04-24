8af2d5af79077ee90ccdb131fa09e6ebe5548742
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-04-25 00:32:15 +0300

    Update Params.js

--- src/AppManager/Params.js
+++ src/AppManager/Params.js
@@ -1,5 +1,5 @@
 export const keyPush = '4d948ff2-ec32-4d5a-af12-bc7cc261454e';
 export const appID = 'id6743004696';
 export const keyApps = 'v7xCW2oiGJ5JauPXwWiS5W';
-export const targetDate = '2025-04-25';
+export const targetDate = '2025-04-27';
 export const bodyLin = 'https://dazzling-paramount-ecstatic.space/3e7wbTD1';
